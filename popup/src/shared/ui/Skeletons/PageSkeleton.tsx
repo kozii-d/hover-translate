8dfb64667a85bc2399d3c8ba4dcde73812342d01
Dmitriy Kozii <dmitriy.kozii@devit.group>
2024-12-16 01:32:37 +0200

    Change page min width. Fix common skeleton

--- popup/src/shared/ui/Skeletons/PageSkeleton.tsx
+++ popup/src/shared/ui/Skeletons/PageSkeleton.tsx
@@ -12,9 +12,11 @@ export const PageSkeleton = () => {
       <Box>
         <Stack spacing={2}>
           <Skeleton variant="rounded" width="100%" height={50} />
+          <Skeleton variant="rounded" width="100%" height={48} />
           <Skeleton variant="rounded" width="100%" height={56} />
           <Skeleton variant="rounded" width="100%" height={56} />
           <Skeleton variant="rounded" width="100%" height={80.83} />
+          <Skeleton variant="rounded" width="100%" height={36.5} />
           <Skeleton variant="rounded" width="100%" height={36.5} />
         </Stack>
       </Box>
