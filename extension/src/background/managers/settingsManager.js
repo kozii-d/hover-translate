048aea2f028ab38e23919fe85c93a7e434989221
Dmitriy Kozii <dmitriy.kozii@devit.group>
2024-12-25 16:44:15 +0200

    Add settingsVersion and tooltipThemeVersion. Add migration logic

--- extension/src/background/managers/settingsManager.js
+++ extension/src/background/managers/settingsManager.js
@@ -4,6 +4,9 @@ export class SettingsManager {
     this.initialize();
   }
 
+  static SETTINGS_VERSION = 1;
+  static TOOLTIP_THEME_VERSION = 1;
+
   initialSettings = {
     sourceLanguageCode: "auto",
     targetLanguageCode: "en",
@@ -21,14 +24,85 @@ export class SettingsManager {
     fontOpacity: "auto",
   };
 
+  settingsMigrations = {
+    // 2: (oldSettings) => {
+    //   return {
+    //     ...oldSettings,
+    //     newFeatureEnabled: true,
+    //   };
+    // },
+  };
+
+  tooltipThemeMigrations = {
+    // 2: (oldTheme) => {
+    //   return {
+    //     ...oldTheme,
+    //     newFeatureEnabled: true,
+    //   };
+    // },
+  };
+
   initialize() {
     chrome.runtime.onInstalled.addListener((details) => {
       if (details.reason === "install") {
         this.initializeSettings();
+        this.initializeTooltipTheme();
+
+        this.storageManager.set("installedAt", Date.now(), "sync");
+        this.storageManager.set("updatedAt", Date.now(), "sync");
+
+        chrome.action.openPopup();
       } else if (details.reason === "update") {
+        this.migrateSettings();
+        this.migrateTooltipTheme();
+
         this.storageManager.set("updatedAt", Date.now(), "sync");
       }
     });
+  }
+
+  async migrateSettings() {
+    let currentVersion = await this.storageManager.get("settingsVersion", "sync") || 0;
+    let settings = await this.storageManager.get("settings", "sync");
+
+    if (!currentVersion || !settings) {
+      this.initializeSettings();
+      return;
+    }
+
+    while (currentVersion < SettingsManager.SETTINGS_VERSION) {
+      if (this.settingsMigrations[currentVersion + 1]) {
+        settings = this.settingsMigrations[currentVersion + 1](settings);
+        currentVersion++;
+      } else {
+        break;
+      }
+    }
+
+    this.storageManager.set("settings", settings, "sync");
+    this.storageManager.set("settingsVersion", SettingsManager.SETTINGS_VERSION, "sync");
+  }
+
+  async migrateTooltipTheme() {
+    let currentVersion = await this.storageManager.get("tooltipThemeVersion", "sync") || 0;
+    let tooltipTheme = await this.storageManager.get("tooltipTheme", "sync");
+
+    if (!currentVersion || !tooltipTheme) {
+      this.initializeTooltipTheme();
+      return;
+    }
+
+    while (currentVersion < SettingsManager.TOOLTIP_THEME_VERSION) {
+      if (this.tooltipThemeMigrations[currentVersion + 1]) {
+        tooltipTheme = this.tooltipThemeMigrations[currentVersion + 1](tooltipTheme);
+        currentVersion++;
+      } else {
+        break;
+      }
+    }
+
+    this.storageManager.set("tooltipTheme", tooltipTheme, "sync");
+    this.storageManager.set("tooltipThemeVersion", SettingsManager.TOOLTIP_THEME_VERSION, "sync");
   }
 
   getUserLanguage() {
@@ -102,10 +176,15 @@ export class SettingsManager {
   };
 
   async initializeSettings() {
-    this.storageManager.set("installedAt", Date.now(), "sync");
-    this.storageManager.set("updatedAt", Date.now(), "sync");
     const initialSettings = await this.getInitialSettings();
     this.storageManager.set("settings", initialSettings, "sync");
+
+    this.storageManager.set("settingsVersion", SettingsManager.SETTINGS_VERSION, "sync");
+  }
+
+  async initializeTooltipTheme() {
     this.storageManager.set("tooltipTheme", this.initialTooltipTheme, "sync");
+
+    this.storageManager.set("tooltipThemeVersion", SettingsManager.TOOLTIP_THEME_VERSION, "sync");
   }
 }
