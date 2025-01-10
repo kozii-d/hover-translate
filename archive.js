37c01717331c68d767b0b7d35e7144245c2adf0b
Dmytro Kozii <dkozii1807@gmail.com>
2025-01-10 15:37:26 +0200

    Fix archive script

--- archive.js
+++ archive.js
@@ -3,8 +3,9 @@ const { execSync } = require("child_process");
 
 try {
   const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+  const manifest = JSON.parse(fs.readFileSync("./manifest.json", "utf8"));
   const name = pkg.name;
-  const version = pkg.version;
+  const version = manifest.version;
   const outputName = `${name}-${version}.zip`;
 
   const exclude = "*.DS_Store";
