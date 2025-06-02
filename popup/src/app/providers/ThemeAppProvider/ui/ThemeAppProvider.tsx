12f265208521532e6d1c645b74135df5fec26646
Dmitriy Kozii <dmitriy.kozii@devit.group>
2025-06-03 01:18:46 +0300

    Refactor project structure and add support modal for cryptocurrency donations

--- popup/src/app/providers/ThemeAppProvider/ui/ThemeAppProvider.tsx
+++ popup/src/app/providers/ThemeAppProvider/ui/ThemeAppProvider.tsx
@@ -3,6 +3,51 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { AppProvider } from "@toolpad/core";
 
+const BUTTONS_COLOR = {
+  contained: {
+    light: {
+      background: "rgb(15, 15, 15)",
+      text: "rgb(255, 255, 255)",
+      hoverText: "rgb(34, 34, 34)",
+      hoverBackground: "rgba(15, 15, 15, 0.8)",
+    },
+    dark: {
+      background: "rgb(241, 241, 241)",
+      text: "rgb(15, 15, 15)",
+      hoverText: "rgb(224, 224, 224)",
+      hoverBackground: "rgba(241, 241, 241, 0.8)",
+    },
+  },
+  outlined: {
+    light: {
+      background: "rgb(15, 15, 15)",
+      text: "rgb(255, 255, 255)",
+      hoverText: "rgb(34, 34, 34)",
+      hoverBackground: "rgba(15, 15, 15, 0.08)",
+    },
+    dark: {
+      background: "rgb(241, 241, 241)",
+      text: "rgb(15, 15, 15)",
+      hoverText: "rgb(224, 224, 224)",
+      hoverBackground: "rgba(241, 241, 241, 0.08)",
+    },
+  },
+  text: {
+    light: {
+      background: "rgb(15, 15, 15)",
+      text: "rgb(255, 255, 255)",
+      hoverText: "rgb(34, 34, 34)",
+      hoverBackground: "rgba(15, 15, 15, 0.08)",
+    },
+    dark: {
+      background: "rgb(241, 241, 241)",
+      text: "rgb(15, 15, 15)",
+      hoverText: "rgb(224, 224, 224)",
+      hoverBackground: "rgba(241, 241, 241, 0.08)",
+    },
+  },
+} as const;
+
 interface ThemeAppProviderProps {
   children: ReactNode;
 }
@@ -10,30 +55,71 @@ interface ThemeAppProviderProps {
 export const ThemeAppProvider: FC<ThemeAppProviderProps> = ({ children }) => {
   const [mode, setMode] = useState<"light" | "dark">("light");
 
-  const theme = useMemo(() => {
-    return createTheme({
-      palette: {
-        mode,
-        primary: {
-          main: "rgb(255, 0, 51)", // MUI primary color
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            main: "rgb(255, 0, 51)",
+          },
         },
-      },
-      components: {
-        MuiButton: {
-          styleOverrides: {
-            root: {
-              "&.MuiButton-containedPrimary:not(:disabled)": {
-                backgroundColor: mode === "dark" ? "#f1f1f1" : "#0f0f0f",
-                color: mode === "dark" ? "#0f0f0f" : "#fff",
-              },
+        components: {
+          MuiButton: {
+            styleOverrides: {
+              root: {
+                "&.MuiButton-contained.MuiButton-colorPrimary": {
+                  backgroundColor:
+                    mode === "light"
+                      ? BUTTONS_COLOR.contained.light.background
+                      : BUTTONS_COLOR.contained.dark.background,
+                  color:
+                    mode === "light"
+                      ? BUTTONS_COLOR.contained.light.text
+                      : BUTTONS_COLOR.contained.dark.text,
+                  "&:hover": {
+                    backgroundColor:
+                      mode === "light"
+                        ? BUTTONS_COLOR.contained.light.hoverBackground
+                        : BUTTONS_COLOR.contained.dark.hoverBackground,
+                  },
+                },
+                "&.MuiButton-outlined.MuiButton-colorPrimary": {
+                  borderColor:
+                    mode === "light"
+                      ? BUTTONS_COLOR.outlined.light.background
+                      : BUTTONS_COLOR.outlined.dark.background,
+                  color:
+                    mode === "light"
+                      ? BUTTONS_COLOR.outlined.light.background
+                      : BUTTONS_COLOR.outlined.dark.background,
+                  "&:hover": {
+                    backgroundColor:
+                      mode === "light"
+                        ? BUTTONS_COLOR.outlined.light.hoverBackground
+                        : BUTTONS_COLOR.outlined.dark.hoverBackground,
+                  },
+                },
 
+                "&.MuiButton-text.MuiButton-colorPrimary": {
+                  color:
+                    mode === "light"
+                      ? BUTTONS_COLOR.text.light.background
+                      : BUTTONS_COLOR.text.dark.background,
+                  "&:hover": {
+                    backgroundColor:
+                      mode === "light"
+                        ? BUTTONS_COLOR.text.light.hoverBackground
+                        : BUTTONS_COLOR.text.dark.hoverBackground,
+                  },
+                },
+              }
             },
           },
         },
-      },
-    });
-  }, [mode]);
-
+      }),
+    [mode]
+  );
   useEffect(() => {
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
     const handleChange = () => {
--- popup/src/pages/AboutPage/ui/AboutPage.tsx
+++ popup/src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,23 +1,34 @@
-import { FC } from "react";
+import { FC, useCallback, useState } from "react";
 import Stack from "@mui/material/Stack";
 import { Page } from "@/shared/ui/Page/Page.tsx";
 import { Footer } from "./Footer.tsx";
-import { Tips } from "@/pages/AboutPage/ui/Tips.tsx";
-import { Contacts } from "./Contacts.tsx";
-import { Description } from "./Description.tsx";
-import { SupportProject } from "./SupportProject.tsx";
+import { Tips } from "@/pages/AboutPage/ui/blocks/Tips.tsx";
+import { Contacts } from "./blocks/Contacts.tsx";
+import { Description } from "./blocks/Description.tsx";
+import { SupportProject } from "./blocks/SupportProject.tsx";
 import Divider from "@mui/material/Divider";
 import { useTranslation } from "react-i18next";
+import { SupportModal } from "@/widgets/SupportModal";
 
 const AboutPage: FC = () => {
   const { t } = useTranslation("about");
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+
+  const openModal = useCallback(() => {
+    setIsOpenModal(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    setIsOpenModal(false);
+  }, []);
 
   return (
     <Page title={t("pageTitle")}>
       <Stack spacing={2}>
+        <SupportModal open={isOpenModal} onClose={closeModal}/>
         <Description />
         <Divider />
-        <SupportProject />
+        <SupportProject openModal={openModal} />
         <Divider />
         <Contacts />
         <Divider />
